180e04bcacf712d2b8c6d09c8c3cf732f80d3f13
elizazh <149685559+elizazh@users.noreply.github.com>
2025-11-06 15:50:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.locals.shopName = 'Thirsty Student';     // <- so header.ejs always has shopName
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({ extended: true }));   // << add this
+app.use(express.urlencoded({ extended: true }));
 
 const mainRoutes = require('./routes/main');
 app.use('/', mainRoutes);
 
-app.listen(port, '0.0.0.0', () => console.log(`Thirsty Student 5a on ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Thirsty Student on ${port}`));
