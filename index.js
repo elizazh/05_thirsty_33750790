ac03578f50f53962c103c920e16d6fbdc81f5357
elizazh <149685559+elizazh@users.noreply.github.com>
2025-11-06 23:30:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,21 @@ app.use('/', mainRoutes);
 app.listen(port, '0.0.0.0', () => {
   console.log(`Thirsty Student on ${port}`);
 });
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.locals.shopName = 'Thirsty Student';
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+
+// ADD THIS so /style.css is served
+app.use(express.static(path.join(__dirname, 'public')));
+
+const mainRoutes = require('./routes/main');
+app.use('/', mainRoutes);
+
+app.listen(port, '0.0.0.0', () => console.log(`Thirsty Student on ${port}`));
